292431e06883cba3d01a913edce9a15c5b3ea360
Aram Papikyan <onlyphilosophylover@gmail.com>
2024-12-22 10:23:04 +0200

    video

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import { HomeTop } from './HomeTop';
 import { HomeExpert } from './HomeExpert';
 import { HomeMeetings } from './HomeMeetings';
 import { HomeConsultation } from './HomeConsultation';
+import { HomeVideo } from './HomeVideo';
 
 export const Home = () => {
     return (
         <>
             <HomeTop />
             <HomeExpert />
+            <HomeVideo/>
             <HomeMeetings />
             <HomeConsultation />
         </>
